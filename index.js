5ddbdb0397900ee93cdc3e9d520fe5b20863d62d
Gabriel Zarate <gabrielzzarate@gmail.com>
2018-02-04 10:54:26 -0500

    added billing and client side app

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	// express will serve up production assets
+	app.use(express.static('client/build'));
+	// express will serve up the index.html file if it doesn't recognize the route so react-router can handle it
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 // PORT : heroku will be able to inject environment variables (variables that can be configured at deployment)
 // boolean handles the development environment
 const PORT = process.env.PORT || 5000;
