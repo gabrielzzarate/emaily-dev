c6a8d6c324bcf7bacde6a939c0d3bbcce697e491
Gabriel Zarate <gabrielzzarate@gmail.com>
2018-02-04 11:19:45 -0500

    debug heroku build

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 	// express will serve up the index.html file if it doesn't recognize the route so react-router can handle it
 	const path = require('path');
+	// is this working
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
